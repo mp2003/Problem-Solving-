29a64fddcdfe29677f475ec41d29c074f9b3b3ae
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-01-24 21:32:43 +0530

    Time: 55 ms (84.08%), Space: 49.1 MB (8.13%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+        return (BigInt(num1) + BigInt(num2)).toString();
+
+};
