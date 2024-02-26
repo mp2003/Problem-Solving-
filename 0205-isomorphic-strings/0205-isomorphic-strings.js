366f1c0c174df7dbc392f25aa294b581eb0035ee
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-02-26 15:37:29 +0530

    Time: 66 ms (42.50%), Space: 51 MB (27.04%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,33 +4,20 @@
  * @return {boolean}
  */
 var isIsomorphic = function (s, t) {
+    let map = new Map(); //S->T
+    let revMap = new Map(); //T->S
 
-    let map = new Map();
-    let revMap = new Map();
-    // S->T
-    for (let i in s) {
+    for (let i = 0; i < s.length; i++) {
         if (map.has(s[i]) || revMap.has(t[i])) {
-            if (t[i] !== map.get(s[i]) || s[i] !== revMap.get(t[i]))
+            if (s[i] !== revMap.get(t[i]) || t[i] !== map.get(s[i])) {
                 return false;
-
+            }
 
         } else {
             map.set(s[i], t[i]);
             revMap.set(t[i], s[i]);
-
         }
     }
 
-    // T->S
-    // for (let i in t) {
-    //     if (revMap.has(t[i])) {
-    //         if (s[i] !== revMap.get(t[i]))
-    //             return false;
-    //         else
-    //             revMap.set(t[i], s[i]);
-    //     } else {
-    //         revMap.set(t[i], s[i]);
-    //     }
-    // }
     return true;
 };
