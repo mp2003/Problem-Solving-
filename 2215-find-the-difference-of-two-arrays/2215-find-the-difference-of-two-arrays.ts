88a843e149f0c4e377c27971cbe08b47465cd4de
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-03-05 22:57:17 +0530

    Time: 96 ms (41.75%), Space: 59.3 MB (6.09%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.ts
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.ts
@@ -1,30 +1,22 @@
 function findDifference(nums1: number[], nums2: number[]): number[][] {
-    let res: number[][] = [];
-    let ans1 = [];
-    let ans2 = [];
-    let map1 = new Map<number, number>();
-    let map2 = new Map<number, number>();
+    let ans1: number[] = [];
+    let ans2: number[] = [];
+    let set1 = new Set<number>([...nums1]);
+    let set2 = new Set<number>([...nums2]);
 
-    for (let val of nums1) {
-        map1.set(val, 1);
-    }
-
-    for (let val of nums2) {
-        map2.set(val, 1);
-    }
-
-    for (let [key,] of map1) {
-        if (!map2.has(key)) {
-            ans1.push(key);
+    set1.forEach((val) => {
+        if (!set2.has(val)) {
+            ans1.push(val)
         }
-    }
+    })
 
-    for (let [key,] of map2) {
-        if (!map1.has(key)) {
-            ans2.push(key);
+
+    set2.forEach((val) => {
+        if (!set1.has(val)) {
+            ans2.push(val)
         }
-    }
-
+    })
+    
     console.log([ans1, ans2]);
     return [ans1, ans2];
 }
