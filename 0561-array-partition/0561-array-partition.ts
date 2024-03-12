b498111e48ceb6bb8b8146bd4dc45aa7c267f777
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-03-12 17:41:08 +0530

    Time: 128 ms (6.01%), Space: 57.6 MB (5.26%) - LeetHub

--- 0561-array-partition/0561-array-partition.ts
+++ 0561-array-partition/0561-array-partition.ts
@@ -1,22 +1,22 @@
-function quickSort(arr: number[]): number[] {
-    if (arr.length <= 1) {
-        return arr;
-    }
+// function quickSort(arr: number[]): number[] {
+//     if (arr.length <= 1) {
+//         return arr;
+//     }
 
-    const pivotIndex = Math.floor(arr.length / 2);
-    const pivot = arr[pivotIndex];
+//     const pivotIndex = Math.floor(arr.length / 2);
+//     const pivot = arr[pivotIndex];
 
-    const left = arr.filter((element, index) => index !== pivotIndex && element < pivot);
-    const equal = arr.filter(element => element === pivot);
-    const right = arr.filter(element => element > pivot);
+//     const left = arr.filter((element, index) => index !== pivotIndex && element < pivot);
+//     const equal = arr.filter(element => element === pivot);
+//     const right = arr.filter(element => element > pivot);
 
-    return [...quickSort(left), ...equal, ...quickSort(right)];
-}
+//     return [...quickSort(left), ...equal, ...quickSort(right)];
+// }
 
 function arrayPairSum(nums: number[]): number {
     // let res: number[][] = [];
     let sum: number = 0;
-    nums = quickSort(nums);
+    nums.sort((a,b)=> a-b);
     console.log(nums);
     for (let i = 0; i < nums.length; i += 2) {
         sum += nums[i];
