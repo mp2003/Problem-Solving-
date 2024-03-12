175b49d711fcccf531c495dbb58c1ff565d75efd
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-03-12 17:40:14 +0530

    Time: 175 ms (6.01%), Space: 67.2 MB (5.26%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.ts
@@ -0,0 +1,25 @@
+function quickSort(arr: number[]): number[] {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const pivotIndex = Math.floor(arr.length / 2);
+    const pivot = arr[pivotIndex];
+
+    const left = arr.filter((element, index) => index !== pivotIndex && element < pivot);
+    const equal = arr.filter(element => element === pivot);
+    const right = arr.filter(element => element > pivot);
+
+    return [...quickSort(left), ...equal, ...quickSort(right)];
+}
+
+function arrayPairSum(nums: number[]): number {
+    // let res: number[][] = [];
+    let sum: number = 0;
+    nums = quickSort(nums);
+    console.log(nums);
+    for (let i = 0; i < nums.length; i += 2) {
+        sum += nums[i];
+    }
+    return sum;
+};
