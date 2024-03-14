9dee95253ce1a740c911a197e6731d016446ef34
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-03-14 14:14:40 +0530

    Time: 71 ms (72.41%), Space: 53.9 MB (65.52%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.ts
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.ts
@@ -1,21 +1,28 @@
-function sumLimit(nums: number[], goal: number): number {
-    if (goal < 0) return 0;
+function limit(nums: number[], goal: number) {
+    if ( goal < 0)
+        return 0;
+    let sum = 0;
+    let i = 0, j = 0, k = 0;
 
-    let sum: number = 0;
-    let i = 0, j = 0;
-    let k = 0;
     while (j < nums.length) {
         sum += nums[j];
+
         while (sum > goal) {
             sum -= nums[i];
             i++;
         }
+
         k += j - i + 1;
         j++;
     }
+
     return k;
 }
 
 function numSubarraysWithSum(nums: number[], goal: number): number {
-    return sumLimit(nums,goal) - sumLimit(nums,goal-1);
+
+    console.log(limit(nums, goal) );
+    console.log(limit(nums, goal-1) );
+
+    return limit(nums, goal) - limit(nums, goal - 1);
 };
