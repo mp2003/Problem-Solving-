84e91dc9b967c6651924173c392fbe44e9bf5a77
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-03-23 12:20:26 +0530

    Time: 51 ms (85.16%), Space: 51.5 MB (58.85%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
@@ -1,9 +1,16 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
-    for (let arr of matrix) {
-        for (let num of arr) {
-            if (target === num)
-                return true;
-        }
+
+    let i = 0, j = matrix[0].length - 1;
+
+    while (i < matrix.length && j >= 0) {
+        let current = matrix[i][j];
+
+        if (current === target) {
+            return true;
+        } else if (current > target)
+            j--;
+        else
+            i++;
     }
 
     return false;
