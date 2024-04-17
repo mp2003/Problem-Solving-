dcd746f47d30d51c737e4e67afccaf545de84c16
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-04-17 20:47:55 +0530

    Time: 44 ms (92.40%), Space: 48.7 MB (51.72%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, millis);
+  });
+}
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
