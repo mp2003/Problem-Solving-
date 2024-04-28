13fe7cc65b72b8bc7e42b185f952c87308d09b9e
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-04-28 11:18:29 +0530

    Time: 51 ms (79.82%), Space: 49.8 MB (28.44%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.js
+++ 2490-circular-sentence/2490-circular-sentence.js
@@ -2,43 +2,17 @@
  * @param {string} sentence
  * @return {boolean}
  */
-
-function splittingFirstAndLastChar(str) {
-  let chArr = [];
-  let arr = str.split(" ");
-  for (let word of arr) {
-    let firstCh = word[0];
-    let lastCh = word[word.length - 1];
-    let ch = [firstCh, lastCh];
-    chArr.push(ch);
-  }
-  return chArr;
-}
-var isCircularSentence = function (str) {
-  let arr = splittingFirstAndLastChar(str);
-  console.log(arr);
-  if (arr.length === 0) return false;
-
-  let bool = false;
-  console.log(arr.length);
-  for (let i = 0; i < arr.length; i++) {
-    console.log(i);
-    if (i != arr.length - 1) {
-      if (
-        arr[i][1] === arr[i + 1][0] &&
-        arr[i][1].charCodeAt(0) === arr[i + 1][0].charCodeAt(0)
-      )
-        bool = true;
-      else return false;
-    } else if (i === arr.length - 1) {
-      if (
-        arr[i][1] === arr[0][0] &&
-        arr[i][1].charCodeAt(0) === arr[0][0].charCodeAt(0)
-      ) {
-        bool = true;
-        console.log(arr[i][1], arr[0][0], "...");
-      } else return false;
+var isCircularSentence = function (s) {
+    for (let i = 0; i < s.length; i++) {
+        if (i != s.length - 1) {
+            if (s[i] === " ") {
+                if (s[i + 1] !== s[i - 1])
+                    return false;
+            }
+        } else {
+            if (s[s.length - 1] !== s[0])
+                return false;
+        }
     }
-  }
-  return bool;
+    return true;
 };
