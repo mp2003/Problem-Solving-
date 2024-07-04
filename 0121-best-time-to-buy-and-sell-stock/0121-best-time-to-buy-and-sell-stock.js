a0502e4ba160e6938dd9322aed40f8846ef26855
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-07-04 20:27:16 +0530

    Time: 87 ms (11.59%), Space: 60.4 MB (8.33%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    let maxProfit = 0;
     let minPrice = Infinity;
+    let maxProfit = 0;
 
-    for (let price of prices) {
-        minPrice = Math.min(price, minPrice);
-        maxProfit = Math.max(price - minPrice, maxProfit);
+    for (let pr of prices) {
+        minPrice = Math.min(minPrice, pr);
+        maxProfit = Math.max(pr - minPrice, maxProfit);
     }
 
     return maxProfit;
