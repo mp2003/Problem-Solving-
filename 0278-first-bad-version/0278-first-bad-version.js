6ccd39b65d69963a7377111d6b22d45c437476a8
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-07-09 23:36:08 +0530

    Time: 42 ms (93.59%), Space: 49.2 MB (5.99%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let l = 1, r = n;
+        while (l < r) {
+            let mid = Math.floor(l + (r - l) / 2);
+            if (! isBadVersion(mid)) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+
+        }
+        return l;
+    };
+};
