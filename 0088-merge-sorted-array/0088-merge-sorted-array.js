86fd9afef9ebe08ed8e4bd20cfb179cdabcfccc7
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-07-10 21:19:05 +0530

    Time: 85 ms (5.40%), Space: 53.9 MB (5.07%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+    let last = m + n - 1;
+
+    while (m>0) {
+        if (nums1[m - 1] <= nums2[n - 1]) {
+            nums1[last] = nums2[n - 1];
+            n--;
+        } else {
+            nums1[last] = nums1[m - 1];
+            m--;
+            console.log(m, nums1[m]);
+        }
+        last--;
+    }
+
+    while (n > 0) {
+        nums1[last] = nums2[n - 1];
+        n--;
+        last--;
+    }
+    return nums1;
+};
