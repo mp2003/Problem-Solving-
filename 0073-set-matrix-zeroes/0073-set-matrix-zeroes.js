148b4e94a6f170a5614c8074b011fb1a4f89e17f
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-07-15 22:31:45 +0530

    Time: 76 ms (62.15%), Space: 53.4 MB (39.29%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -3,41 +3,42 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function (matrix) {
-    let ROWS = matrix.length, COLS = matrix[0].length;
-    let rowZero = false;
-
-    for (let r = 0; r < ROWS; r++) {
-        for (let c = 0; c < COLS; c++) {
-            if (matrix[r][c] === 0) {
+    let cols = matrix[0].length, rows = matrix.length;
+    let firstRow = false;
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (matrix[r][c] == 0) {
                 matrix[0][c] = 0;
-
-                if (r > 0)
-                    matrix[r][0] = 0;
-                else
-                    rowZero = true;
+                if (r > 0) {
+                    matrix[r][0] = 0
+                } else {
+                    firstRow = true;
+                }
             }
         }
     }
 
 
-    for (let r = 1; r < ROWS; r++) {
-        for (let c = 1; c < COLS; c++) {
-            if (matrix[0][c] === 0 || matrix[r][0]===0)
+    for (let r = 1; r < rows; r++) {
+        for (let c = 1; c < cols; c++) {
+            if (matrix[0][c] == 0 || matrix[r][0] == 0) {
                 matrix[r][c] = 0;
+            }
         }
     }
 
-    if (matrix[0][0] === 0)
-        for (let r = 0; r < ROWS; r++) {
+    if (matrix[0][0] == 0) {
+        for (let r = 0; r < rows; r++) {
             matrix[r][0] = 0;
         }
+    }
 
-    if (rowZero) {
-        for (let c = 0; c < COLS; c++) {
+    if (firstRow) {
+        for (let c = 0; c < cols; c++) {
             matrix[0][c] = 0;
         }
     }
 
-    return matrix;
 
+    return matrix;
 };
