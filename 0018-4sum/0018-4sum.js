19a1ebaa9bdd5075684140ce18e8eb165145143f
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-07-22 23:55:25 +0530

    Time: 84 ms (68.37%), Space: 53.7 MB (39.15%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -4,29 +4,24 @@
  * @return {number[][]}
  */
 var fourSum = function (nums, target) {
-    nums.sort((a, b) => a - b);
-    // console.log(nums);
     let ans = [];
+    nums.sort((a, b) => a - b);
+
     for (let i = 0; i < nums.length; i++) {
         let a = nums[i];
-        if (i > 0 && a === nums[i - 1])
-            continue;
+        if (i > 0 && a === nums[i - 1]) continue;
         for (let j = i + 1; j < nums.length; j++) {
             let b = nums[j];
-            if (j > i+1 && b === nums[j - 1])
-            continue;
+            if (j > i + 1 && b === nums[j - 1]) continue;
             let l = j + 1, r = nums.length - 1;
             while (l < r) {
                 let sum = a + b + nums[l] + nums[r];
-                // console.log([a,b,nums[l],nums[r]],sum)
-                if (sum > target)
-                    r--;
-                else if (sum < target)
-                    l++;
+                if (sum < target) l++;
+                else if (sum > target) r--;
                 else {
                     ans.push([a, b, nums[l], nums[r]]);
                     l++;
-                    while (nums[l] === nums[l - 1] && l < r) {
+                    while (l < r && nums[l] === nums[l - 1]) {
                         l++;
                     }
                 }
