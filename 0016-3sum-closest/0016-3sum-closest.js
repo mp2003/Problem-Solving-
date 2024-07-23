bfd3f41c1f17774322dc7eda7b06ebd4c264635f
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-07-23 23:24:06 +0530

    Time: 72 ms (52.73%), Space: 50.9 MB (51.85%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -5,21 +5,27 @@
  */
 var threeSumClosest = function (nums, target) {
     nums.sort((a, b) => a - b);
-    let minDiff = Infinity;
-    let ans = 0;
+    let diff = Infinity, ans = 0;
     for (let i = 0; i < nums.length; i++) {
         let a = nums[i];
-        if (a == nums[i - 1] && i > 0) continue;
+        if (i > 0 && a === nums[i - 1]) continue;
         let l = i + 1, r = nums.length - 1;
         while (l < r) {
             let sum = a + nums[l] + nums[r];
-            if (minDiff > Math.abs(sum - target)) {
-                minDiff = Math.abs(sum - target);
+            if (diff > Math.abs(target - sum)) {
+                diff = Math.abs(target - sum);
                 ans = sum;
             }
+            // console.log("a:", a, "b:", nums[l], "c:", nums[r], "diff", diff);
+
             if (sum > target) r--;
             else if (sum < target) l++;
-            else return sum;
+            else {
+                return sum;
+                l++;
+                while (l < r && nums[l] === nums[l - 1]) l++;
+            }
+            // ans = target - diff;
         }
     }
     return ans;
