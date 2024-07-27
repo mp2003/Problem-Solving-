172cfb55cf7d2c5f09411eb3d563adf9b957acd6
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-07-28 00:17:18 +0530

    Time: 56 ms (39.55%), Space: 50.5 MB (15.67%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areNumbersAscending = function (s) {
+    let numArr = [];
+    let ans = false;
+    s.split(" ").forEach((el) => {
+        if (el !== " " && !isNaN(el)) numArr.push(Number(el));
+    })
+    console.log(numArr)
+
+    for (let i = 0; i < numArr.length - 1; i++) {
+        if (numArr[i] < numArr[i + 1]) {
+            ans = true;
+        }
+        else {
+            return false;
+        };
+    }
+    return ans;
+};
