ae3b0bc06f41021bd8c022ffccc475c8b276684c
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-07-28 00:45:07 +0530

    Time: 48 ms (79.71%), Space: 48.5 MB (84.35%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortSentence = function (s) {
+    let arr = s.split(" ").map((el) => {
+        let num = el.split("").filter((w) => !isNaN(w) && w.trim() !== '');
+        return [Number(num), el.slice(0, el.length - 1)];
+    });
+
+    arr.sort((a, b) => a[0] - b[0]);
+    // console.log(arr); 
+
+    let ans = [];
+
+    for (let a of arr) {
+        ans.push(a[1]);
+    }
+    // console.log(ans);
+
+    return ans.join(" ");
+};
