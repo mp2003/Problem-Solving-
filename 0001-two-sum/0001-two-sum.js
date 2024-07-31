49647c614f200dd5a9b71a9aa23fa7f9ac16df63
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-07-31 19:50:52 +0530

    Time: 61 ms (66.70%), Space: 51.9 MB (11.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,14 +5,11 @@
  */
 var twoSum = function (nums, target) {
     let map = new Map();
-    for (let i in nums) {
-        map.set(nums[i], i);
-    }
-
-    for (let i in nums) {
-        let comp = target - nums[i];
-        if (map.has(comp) && map.get(comp) !== i) {
-            return [i, map.get(comp)];
+    for (let i = 0; i < nums.length; i++) {
+        let a = target - nums[i];
+        if (map.has(a)) {
+            return [i, map.get(a)];
         }
+        map.set(nums[i], i)
     }
 };
