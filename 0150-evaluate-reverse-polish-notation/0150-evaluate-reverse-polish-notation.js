3ed226c2f1d3e4edcdd65e29b7ea798f992fc62c
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-08-24 01:05:58 +0530

    Time: 87 ms (18.43%), Space: 59.1 MB (13.85%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -9,12 +9,9 @@ var evalRPN = function (tokens) {
       let num1 = stack.pop();
       let num2 = stack.pop();
       let ans = eval(`${num2} ${ch} ${num1}`);
-      //   console.log(Math.ceil(ans));
       ans = ans >= 0 ? Math.floor(ans) : Math.ceil(ans);
       stack.push(Math.ceil(ans));
     } else stack.push(ch);
   }
-//   console.log(stack[0]);
-
   return stack[0];
 };
