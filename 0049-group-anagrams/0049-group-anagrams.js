fbb36604bec526c93e967b65d6acc2211e910c77
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-08-26 23:44:26 +0530

    Time: 115 ms (56.02%), Space: 62.5 MB (66.11%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -6,11 +6,12 @@ var groupAnagrams = function (strs) {
     let map = new Map();
     for (let str of strs) {
         let sortedStr = str.split("").sort().join("");
-        if (map.has(sortedStr)) {
-            map.get(sortedStr).push(str);
-        } else {
+        if (!map.has(sortedStr)) {
             map.set(sortedStr, [str]);
-        }
+        } else
+            map.get(sortedStr).push(str);
+        // map.set(sortedStr, arr.push(str));
     }
+    // console.log(map);
     return Array.from(map.values());
 };
