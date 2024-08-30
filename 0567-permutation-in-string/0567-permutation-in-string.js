9537bf0ddca3a0f03aacf4b104f2eff987671d03
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-08-31 00:09:55 +0530

    Time: 70 ms (73.27%), Space: 52.6 MB (73.63%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -5,25 +5,28 @@
  */
 var checkInclusion = function (s1, s2) {
     let map = new Map();
-    for (let ch of s1) {
+    for (const ch of s1) {
         map.set(ch, (map.get(ch) || 0) + 1);
     }
-    let i = 0, j = 0, k = s1.length, count = map.size;
+
+    let i = 0, j = 0, count = map.size;
     while (j < s2.length) {
+        // console.log(map, s2[j], s2[i], map.get(s2[i]));
+
         if (map.has(s2[j])) {
             map.set(s2[j], map.get(s2[j]) - 1);
-            if (map.get(s2[j]) === 0) {
-                count--;
-            }
+            if (map.get(s2[j]) == 0) count--;
         }
 
-        if (j - i + 1 === k) {
+        if (j - i + 1 === s1.length) {
             if (count === 0) return true;
 
             if (map.has(s2[i])) {
-                map.set(s2[i], map.get(s2[i]) + 1);
+                map.set(s2[i], map.get(s2[i]) + 1)
+                if (map.get(s2[i]) == 1) {
+                    count++;
+                }
 
-                if (map.get(s2[i]) === 1) count++;
             }
             i++;
         }
