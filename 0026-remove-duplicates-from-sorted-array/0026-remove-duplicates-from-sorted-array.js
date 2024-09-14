91aead09e508c102bed5efe135c832b94cd4a70e
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-09-14 17:57:00 +0530

    Time: 68 ms (40.67%), Space: 51.7 MB (91.94%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,10 +5,10 @@
 var removeDuplicates = function (nums) {
     let k = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== nums[k]) {
-            k++;
+        if (nums[i] !== nums[i + 1]) {
             nums[k] = nums[i];
+            k++;
         }
     }
-    return k + 1;
+    return k;
 };
