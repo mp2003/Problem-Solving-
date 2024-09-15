325a4ec65bb5f9051a2ce997910b9c43d7792a00
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-09-15 20:52:58 +0530

    Time: 82 ms (85.97%), Space: 73.8 MB (15.43%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let map = new Map();
+    
+    for( let i=0; i<nums.length; i++){
+        if(map.has(nums[i]) && Math.abs(i- map.get(nums[i]))<=k){
+            return true;
+        }
+        map.set(nums[i],i);
+    }
+    return false;
+};
