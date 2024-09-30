1dfb732e180477541eebd7e5f83a7c3fc91eab34
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-09-30 11:28:02 +0530

    Time: 59 ms (18.05%), Space: 48.7 MB (62.41%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.js
+++ 0856-score-of-parentheses/0856-score-of-parentheses.js
@@ -2,16 +2,16 @@
  * @param {string} s
  * @return {number}
  */
-var scoreOfParentheses = function (s) {
-    let stack = [], score = 0;
-    for (let ch of s) {
-        if (ch === "(") {
-            stack.push(0);
+var scoreOfParentheses = function(s) {
+    let stack = [];
+    let score = 0;
+    for ( let ch of s){
+        if ( ch === ")"){
+            let lastCh = stack.pop();
+            let innerSc = lastCh === 0 ? 1 : 2 * lastCh;
+            stack.length === 0 ? score += innerSc : stack[stack.length-1] += innerSc;
         } else {
-            let last = stack.pop();
-            let currentScore = last === 0 ? 1 : 2 * last;
-            stack.length === 0 ? score += currentScore :
-                stack[stack.length - 1] += currentScore;
+            stack.push(0);
         }
     }
     return score;
