d7cbd14bbbe877c14d3936cb22735f1e2694f6c1
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2024-10-28 21:44:42 +0530

    Time: 10 ms (45.42%), Space: 57.8 MB (57.93%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -1,19 +1,7 @@
-var isPalindrome = function (x) {
-  if (x < 0) return false;
-
-  let revNum = 0,
-    temp = x,
-    pow = 0;
-  while (temp > 0) {
-    temp = Math.floor(temp / 10);
-    pow++;
-  }
-  temp = x;
-  while (temp > 0) {
-    revNum += (temp % 10) * Math.pow(10, --pow);
-    temp = Math.floor(temp / 10);
-  }
-return revNum === x
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    return x.toString() === x.toString().split("").reverse().join("");
 };
-
-
